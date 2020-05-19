b5f77ba8be8d0f860e7133cafaa872937043973b
Chad Palmer <chad.palmer@yale.edu>
2020-05-19 19:06:16 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -6,9 +6,14 @@ def consolidate_cart(cart)
   cart.each_with_object([]) do |hash, sorted_cart|
     if find_item_by_name_in_collection(hash[:item], sorted_cart)
       sorted_cart.each do |item|
-        if item[:name]
+        if item[:item] == hash[:item]
+          item[:count] += 1
+        end
+      end
     else
       updated_item = {}.update(hash)
       updated_item[:count] = 1
       sorted_cart.push(updated_item)
+    end
+  end
 end
